8f5110c4532a203a40fa782d5b3d6a889eb15667
Arie Trouw <arie.trouw@xyfindit.com>
2017-05-14 18:37:27 -0700

    fabric

--- ui-android-sample/src/main/java/com/xyfindables/ui/sample/XYFindablesUISampleActivity.java
+++ ui-android-sample/src/main/java/com/xyfindables/ui/sample/XYFindablesUISampleActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.xyfindables.ui.views.XYToolbar;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class XYFindablesUISampleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_xyfindables_ui_sample);
     }
 
